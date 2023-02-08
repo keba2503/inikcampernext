3d69031904b74295144086d8ce47e147b5bb9b35
Karen <karen.borrero@grupodinosol.es>
2023-02-08 10:30:46 +0000

    titulo mapa

--- src/components/Home/MapSection.js
+++ src/components/Home/MapSection.js
@@ -5,7 +5,21 @@ class Map extends Component {
     render() {
 
         return (
+
+
+
             <div className="map-area">
+                <div className="section-title-wrapper mb-45 text-center">
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <h3 className="section-title mb-35">Mapa Recomendado - Gran Canaria</h3>
+                </div>
                 <iframe src="https://www.google.com/maps/d/embed?mid=1CCn7O6uUoBbWwVTTF5EJW7-MZxV49po&hl=es&ehbc=2E312F"  loading="lazy"></iframe>
             </div>
         );

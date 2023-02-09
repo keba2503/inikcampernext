c3d516a3a73af87a047d19e6aac95f02a0494ab5
Karen <karen.borrero@grupodinosol.es>
2023-02-09 07:52:47 +0000

    Formulario de contacto en service

--- src/pages/service.js
+++ src/pages/service.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import Header from '../components/Layout/Header/Header';
 import Footer from '../components/Layout/Footer/Footer';
-import ServiceFour from '../components/Service/ServiceSectionFour';
 import ServiceThree from '../components/Service/ServiceSectionThree';
-import WhyWeThree from '../components/Service/WhyWeThree';
 import dynamic from 'next/dynamic';
 import Breadcrumb from '../components/Common/Breadcrumb';
+import {Formfaq} from '../components/Contact/formfaq'
 
 
 const BrandFour = dynamic(() => import('../components/Elements/Brand/BrandSectionFour'), {
     ssr: false
-  })
+})
 
 class ServicePage extends React.Component {
 
-    static getInitialProps({store}) {}
+    static getInitialProps({store}) {
+    }
 
     constructor(props) {
         super(props);
@@ -23,21 +23,11 @@ class ServicePage extends React.Component {
     render() {
         return (
             <React.Fragment>
-            <Header />
-            <Breadcrumb pageTitle="Servicios" />
-
-            <ServiceThree />
-				{/* service-end */}
-                {/* whywe-start */}
-                {/* <WhyWeThree /> */}
-				{/* whywe-end */}
-                {/* service-start */}
-				{/* <ServiceFour /> */}
-				{/* service-end */}
-                {/* brand-start */}
-                {/* <BrandFour /> */}
-				{/* brand-end */}               
-            <Footer />
+                <Header/>
+                <Breadcrumb pageTitle="Servicios"/>
+                <ServiceThree/>
+                <Formfaq/>
+                <Footer/>
             </React.Fragment>
         );
     }

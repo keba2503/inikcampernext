81bb0aae55010a190df7e3b0495137bbed25a098
Karen <karen.borrero@grupodinosol.es>
2023-03-19 19:38:10 +0000

    delete console.log

--- src/pages/api/admin.js
+++ src/pages/api/admin.js
@@ -17,7 +17,6 @@ export default async function handler(req, res) {
     if (req.method === 'GET') {
         try {
             const datos = await getDatos();
-            console.log(datos)
             res.status(200).json(datos);
         } catch (error) {
             console.error(error);

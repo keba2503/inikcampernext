a2cc365a95f91e72f44249566c8224f54eac2d7c
Karen <karen.borrero@grupodinosol.es>
2023-03-24 18:59:57 +0000

    link y text adicional guideUser

--- src/components/Faq/Guie.js
+++ src/components/Faq/Guie.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Guie extends Component {
     state = {
@@ -7,9 +7,9 @@ class Guie extends Component {
 
     fetchGuideData = async () => {
         try {
-            const response = await fetch('https://inikcamper.es/api/admin');
+            const response = await fetch('http://localhost:3000/api/admin');
             const data = await response.json();
-            this.setState({ guideData: data});
+            this.setState({guideData: data});
         } catch (error) {
             console.log(error);
         }
@@ -28,23 +28,36 @@ class Guie extends Component {
                         <div className="col-lg-12">
                             <div className="faq-wrapper">
                                 <h2>Guía de usuario</h2>
-                                <br />
+                                <br/>
                                 <div className="choose-right" data-aos="fade-left" data-aos-duration="1000">
                                     <div className="accordion" id="accordionExample">
                                         {this.state.guideData.map(guide => (
                                             <div className="accordion-item" key={guide["id"]}>
                                                 <h2 className="accordion-header" id="headingTwo">
-                                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse" + guide.id} aria-expanded="false" aria-controls="collapseTwo">
+                                                    <button className="accordion-button collapsed" type="button"
+                                                            data-bs-toggle="collapse"
+                                                            data-bs-target={"#collapse" + guide.id}
+                                                            aria-expanded="false" aria-controls="collapseTwo">
                                                         {guide.nombre}
                                                     </button>
                                                 </h2>
-                                                <div id={"collapse" + guide.id} className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                                <div id={"collapse" + guide.id} className="accordion-collapse collapse"
+                                                     aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                                                     <div className="accordion-body">
                                                         <p className='Text-guie'>
                                                             {guide.texto_inicial}
                                                         </p>
                                                         <p className='Text-guie'>
-                                                            {guide.texto_adicional}                                                        </p>
+                                                            {guide.texto_adicional}
+                                                        </p>
+                                                        <p className='Text-guie'>
+                                                            {guide.texto_adicional2}
+                                                        </p>
+                                                        <p className='Text-guie'>
+                                                            {guide.texto_adicional3}
+                                                        </p>
+                                                        <a href={guide.link_reference}
+                                                           target="_blank">{guide.link_reference}</a>
                                                     </div>
                                                 </div>
                                             </div>

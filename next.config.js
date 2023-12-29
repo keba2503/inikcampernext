a363f5b11e7cf994df10ef85e5fdb0fbec770514
Karen <karen.borrero@grupodinosol.es>
2023-12-29 20:17:28 +0000

    SEO

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ module.exports = {
             {
                 source: '/:path*',
                 destination: '/:path*',
-                permanent: true,
+                permanent: false,
             },
         ];
     },

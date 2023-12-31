7dacd15c658bdedfa9fe897c5578ffee3b5478fb
Karen <karen.borrero@grupodinosol.es>
2023-12-31 12:46:57 +0000

    seo footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,64 +1,66 @@
 import React from 'react';
 import FooterBottom from './FooterBottom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 
 const Footer = () => {
 
-   return (
+    return (
 
-      <footer style={{ background: 'linear-gradient(to right, #c58940 0%, #d3c199 100%' }} className="pt-95 position-relative">
-         <div className="common-shape-wrapper wow slideInRight animated" data-wow-delay="0ms" data-wow-duration="1500ms">
-            <div className="common-shape-inner wow slideInRight animated" data-wow-delay="0ms" data-wow-duration="1500ms"></div>
-         </div>
-         <div className="footer-area pb-60">
-            <div className="container">
-               <div className="row">
-                  <div className="col-lg-4 col-sm-6">
-                     <div className="widget mb-30">
+        <footer style={{background: 'linear-gradient(to right, #c58940 0%, #d3c199 100%'}} className="pt-95 position-relative">
+            <div className="footer-area pb-60">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-4 col-sm-6">
+                            <div className="widget mb-30">
 
-                        <div className="footer-logo mb-25">
-                           <Link href="/"><a><img src="assets/img/logo/logo-white.png" className="img-fluid" alt="footer-logo" /></a></Link>
+                                <div className="footer-logo mb-25">
+                                    <Link href="/"><a><img src="assets/img/logo/logo-white.png" className="img-fluid" alt="footer-logo"/></a></Link>
+                                </div>
+                                <div className="footer-social">
+                                    <a href="contact" target="_blank">
+                                        <h4 className="widget-title mb-35">Contactanos</h4></a>
+                                    <a href="https://www.twitter.com/inikcamper" target="_blank"><i><FontAwesomeIcon icon={['fab', 'twitter']}/></i></a>
+                                    <a href="https://www.facebook.com/InikCamper-113925204533049" target="_blank"><i><FontAwesomeIcon icon={['fab', 'facebook']}/></i></a>
+                                    <a href="https://wa.me/34684198547" target="_blank"><i><FontAwesomeIcon icon={['fab', 'whatsapp']}/></i></a>
+                                    <a href="https://www.instagram.com/inikcamper.es" target="_blank"><i><FontAwesomeIcon icon={['fab', 'instagram']}/></i></a>
+                                </div>
+                            </div>
                         </div>
-                        <div className="footer-social">
-                          <a href="contact" target="_blank"><h4 className="widget-title mb-35">Contactanos</h4></a>
-                           <a href="https://www.twitter.com/inikcamper" target="_blank"><i><FontAwesomeIcon icon={['fab', 'twitter']} /></i></a>
-                           <a href="https://www.facebook.com/InikCamper-113925204533049" target="_blank"><i><FontAwesomeIcon icon={['fab', 'facebook']} /></i></a>
-                           <a href="https://wa.me/34684198547" target="_blank"><i><FontAwesomeIcon icon={['fab', 'whatsapp']} /></i></a>
-                            <a href="https://www.instagram.com/inikcamper.es" target="_blank"><i><FontAwesomeIcon icon={['fab', 'instagram']}/></i></a>
+
+                        <div className="col-lg-2 col-sm-6">
+                            <div className="widget mb-30">
+                                <h4 className="widget-title mb-35">Links</h4>
+                                <ul>
+                                    <li><Link href="/service"><a>Servicios</a></Link></li>
+                                    <li><Link href="/about"><a>Equipo</a></Link></li>
+                                    <li><Link href="/booking"><a>Reservas</a></Link></li>
+                                    <li><Link href="/contact"><a>Escribenos - contacto</a></Link></li>
+                                </ul>
+                            </div>
                         </div>
-                     </div>
-                  </div>
+                        <div className="col-lg-3 col-sm-6">
 
-                  <div className="col-lg-2 col-sm-6">
-                     <div className="widget mb-30">
-                        <h4 className="widget-title mb-35">Links</h4>
-                        <ul>
-                           <li><Link href="/service"><a>Servicios</a></Link></li>
-                           <li><Link href="/about"><a>Equipo</a></Link></li>
-                           <li><Link href="/booking"><a>Reservas</a></Link></li>
-                           <li><Link href="/contact"><a>Escribenos - contacto</a></Link></li>
-                        </ul>
-                     </div>
-                  </div>
-                  <div className="col-lg-3 col-sm-6">
+                            <div className="widget widget-contact mb-30">
+                                <h2 className="widget-title mb-35">Contacto</h2>
+                                <ul>
+                                    <li className="pb-10">Gran Canaria - España <br/></li>
+                                    <li>
+                                        <img src="assets/img/icon/footer-icon-1.png" className="img-fluid" alt="icon-img"/><a href="mailto:inikcamper@gmail">inikcamper@gmail.com</a>
+                                    </li>
+                                    <li>
+                                        <img src="assets/img/icon/footer-icon-2.png" className="img-fluid" alt="icon-img"/><a href="tel:+34-684198547">+34-684198547</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
 
-                     <div className="widget widget-contact mb-30">
-                        <h2 className="widget-title mb-35">Contacto</h2>
-                        <ul>
-                           <li className="pb-10">Gran Canaria - España <br /></li>
-                           <li><img src="assets/img/icon/footer-icon-1.png" className="img-fluid" alt="icon-img" /><a href="mailto:inikcamper@gmail">inikcamper@gmail.com</a></li>
-                           <li><img src="assets/img/icon/footer-icon-2.png" className="img-fluid" alt="icon-img" /><a href="tel:+34-684198547">+34-684198547</a></li>
-                        </ul>
-                     </div>
-                  </div>
-
-               </div>
+                    </div>
+                </div>
             </div>
-         </div>
-         <FooterBottom />
-      </footer>
-   );
+            <FooterBottom/>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/components/Layout/Footer/FooterBottom.js
+++ src/components/Layout/Footer/FooterBottom.js
@@ -3,16 +3,12 @@ import React from 'react';
 const FooterBottom = () => {
     return (
         <div className="copyright-area">
-            <div className="container">
-                <div className="copyright-bg">
-                    <div className="row align-items-center">
-                        <div className="col-md-6">
+            <div className="copyright-bg">
+                <div className="row align-items-center">
+                    <div className="col-md-6">
+                        <div className="container">
                             <div className="copyright">
                                 <span>Copyright ©2022 Karen Borrero. All Rights Reserved</span>
-                            </div>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="privacy-text text-center text-md-end">
                             </div>
                         </div>
                     </div>
